71544d4d52c11a33a114e75322a2e0eb5da08bd1
seiyo <seiyo.03oo@gmail.com>
2025-06-22 04:32:22 +0900

    rangesetのバグ修正とsize関数追加

--- data_structures/rangeset.hpp
+++ data_structures/rangeset.hpp
@@ -1,4 +1,4 @@
-// https://github.com/seiyo1/kyopro-library/blob/main/data_structures/rangeset.hpptemplate <typename T>
+// https://github.com/seiyo1/kyopro-library/blob/main/data_structures/rangeset.hpp
 template <typename T>
 struct rangeset {
   set<pair<T, T>> s;
@@ -20,7 +20,7 @@ struct rangeset {
     T LL = -1, LR = -1, RL = -1, RR = -1;
     auto itL = s.upper_bound({L, iinf});
     auto itR = s.upper_bound({R, iinf});
-    if (itL != s.begin() && (--itL)->second < L) ++itL;
+    if (itL != s.begin() && (--itL)->second <= L) ++itL;
     if (itL != itR) {
       if (itL->first < L) {
         LL = itL->first;
@@ -35,14 +35,13 @@ struct rangeset {
     if (LL != -1) s.insert({LL, LR});
     if (RR != -1) s.insert({RL, RR});
   }
-
+  // [1,2) [2,3)はNo.
   bool hasOverlap(T L, T R) {
-    auto itL = s.upper_bound({L, iinf});
-    auto itR = s.upper_bound({R, iinf});
-    if (itL != s.begin() && (--itL)->second < L) ++itL;
-    if (itL != itR) {
-      return true;
-    }
-    return false;
+    auto it = s.lower_bound({R, numeric_limits<T>::lowest()});
+    if (it == s.begin()) return false;
+    --it;
+    return it->second > L;
   }
+
+  unsigned int size() { return s.size(); }
 };
