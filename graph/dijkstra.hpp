b954b3037143a09f66538730857d6f817311bf64
seiyo <seiyo.03oo@gmail.com>
2025-07-05 14:30:18 +0900

    graphとダイクストラを追加

---
+++ graph/dijkstra.hpp
@@ -0,0 +1,24 @@
+template<typename T>
+vector<T> dijkstra(const vvec<P>& g, int start) {
+    int n = g.size();
+    const T INF = numeric_limits<T>::max();
+    vector<T> dist(n, INF);
+    using P = pair<T, int>;
+    priority_queue<P, vector<P>, greater<P>> pq;
+
+    dist[start] = 0;
+    pq.emplace(0, start);
+    while (!pq.empty()) {
+        auto [d, v] = pq.top();
+        pq.pop();
+        if (d > dist[v]) continue;
+        for (auto& [to, cost] : g[v]) {
+            T nd = d + cost;
+            if (nd < dist[to]) {
+                dist[to] = nd;
+                pq.emplace(nd, to);
+            }
+        }
+    }
+    return dist;
+}
