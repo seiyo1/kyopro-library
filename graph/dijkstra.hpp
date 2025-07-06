786e32b7be926296192175c726d12d2a5965a591
seiyo <seiyo.03oo@gmail.com>
2025-07-06 22:05:05 +0900

    pair<ll,ll>に統一

--- graph/dijkstra.hpp
+++ graph/dijkstra.hpp
@@ -1,20 +1,18 @@
 // https://github.com/seiyo1/kyopro-library/blob/main/graph/dijkstra.hpp
-template <typename T>
-vector<T> dijkstra(const vector < vector<pair<int, T>> & g, int start) {
-  int n = g.size();
-  const T INF = numeric_limits<T>::max();
-  vector<T> dist(n, INF);
-  using P = pair<T, int>;
+vector<ll> dijkstra(const vector<vector<P>> &g, ll start) {
+  int n = (int)g.size();
+  vector<ll> dist(n, INF);
   priority_queue<P, vector<P>, greater<P>> pq;
-
   dist[start] = 0;
   pq.emplace(0, start);
   while (!pq.empty()) {
     auto [d, v] = pq.top();
     pq.pop();
     if (d > dist[v]) continue;
-    for (auto& [to, cost] : g[v]) {
-      T nd = d + cost;
+    for (auto &e : g[v]) {
+      ll to = e.first;
+      ll cost = e.second;
+      ll nd = d + cost;
       if (nd < dist[to]) {
         dist[to] = nd;
         pq.emplace(nd, to);
